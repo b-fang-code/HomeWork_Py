60929529e211a082b8be6c0d41cab31d2eefee91
b_fang <shampuk@gmail.com>
2023-02-14 17:55:30 +0300

    Add new solution for HomeWork_Py_004

--- HomeWork_Py_004/task01.py
+++ HomeWork_Py_004/task01.py
@@ -7,29 +7,49 @@
 # 2 4 6 8 10 12 10 8 6 4 2
 # 3 6 9 12 15 18
 # 6 12
+import random
 
+# quantity_1 = int(input('Введите количество элементов первого множества: '))
+# quantity_2 = int(input('Введите количество элементов второго множества: '))
+#
+# list_1 = []
+# list_2 = []
+#
+# for i in range(quantity_1):
+#     list_1.append(int(input(f'Введите {i + 1} элемент первого множества: ')))
+# print('Достаточно! ')
+#
+# for i in range(quantity_2):
+#     list_2.append(int(input(f'Введите {i + 1} элемент второго  множества: ')))
+# print('Достаточно! ')
+#
+#
+# print(f'Первое множество: {list_1}')
+# print(f'Второе множество: {list_2}')
+# list_1 = set(list_1)
+# list_2 = set(list_2)
+#
+# list_3 = list_1.intersection(list_2)
+# list_3 = list(list_3)
+# list_3.sort()
+# print(f'Повторяющиеся числа: {list_3}')
 
-quantity_1 = int(input('Введите количество элементов первого множества: '))
-quantity_2 = int(input('Введите количество элементов второго множества: '))
+# -------------------------------------------------------------------------------------------------------------
+#  2 СПОСОБ:
 
-list_1 = []
-list_2 = []
+first_size = int(input('Введите размер первого списка: '))
+second_size = int(input('Введите размер второго списка: '))
 
-for i in range(quantity_1):
-    list_1.append(int(input(f'Введите {i + 1} элемент первого множества: ')))
-print('Достаточно! ')
+first_list = [random.randint(0, 20) for _ in range(first_size)]
+second_list = [random.randint(0, 20) for _ in range(second_size)]
 
-for i in range(quantity_2):
-    list_2.append(int(input(f'Введите {i + 1} элемент второго  множества: ')))
-print('Достаточно! ')
+print(first_list)
+print(second_list)
 
+final_list = []
+for item in first_list:
+    if item in second_list:
+        final_list.append(item)
 
-print(f'Первое множество: {list_1}')
-print(f'Второе множество: {list_2}')
-list_1 = set(list_1)
-list_2 = set(list_2)
-
-list_3 = list_1.intersection(list_2)
-list_3 = list(list_3)
-list_3.sort()
-print(f'Повторяющиеся числа: {list_3}')
+final_list.sort()
+print(final_list)
